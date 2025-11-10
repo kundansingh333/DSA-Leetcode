88d223c6bfdf9e36b75b2b002d4fdbfd52a3166e
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2025-11-10 21:01:44 +0530

    Time: 46 ms (70.76%) | Memory: 56.7 MB (31.14%) - LeetSync

---
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let value=init;
+    if(nums.length===0) return init;
+    
+    for(let i=0; i<nums.length; i++){
+        value=fn(value,nums[i]);
+    }
+    return value;
+
+};
