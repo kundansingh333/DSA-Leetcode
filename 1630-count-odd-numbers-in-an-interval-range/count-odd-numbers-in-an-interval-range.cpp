d64e188d863a744e7c29c4801dc496ecdd4c57e1
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2025-12-07 08:47:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (69.29%) - LeetSync

--- 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.cpp
+++ 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.cpp
@@ -9,17 +9,19 @@ public:
         // }
         // return count;
 
-        int N=high-low+1;
-        if(N%2==0){
-            return N/2;
-        }else{
-            int n=N/2;
-            if(low%2!=0){
-                return n+1;
-            }else{
-                return n;
-            }
-        }
-        return 0;
+        // int N=high-low+1;
+        // if(N%2==0){
+        //     return N/2;
+        // }else{
+        //     int n=N/2;
+        //     if(low%2!=0){
+        //         return n+1;
+        //     }else{
+        //         return n;
+        //     }
+        // }
+        // return 0;
+        return (high+1)/2-low/2;
+
     }
 };
