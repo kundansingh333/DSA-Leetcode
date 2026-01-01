6e9b5fe1fae09f18946f45a23bb7985844f49222
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-01-01 09:41:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (50.17%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int size=digits.size()-1;
-        for(int i=size; i>=0; i--){
+        // int size=digits.size()-1;
+        // for(int i=size; i>=0; i--){
+        //     if(digits[i]<9){
+        //         digits[i]++;
+        //         return digits;
+        //     }
+        //     digits[i]=0;
+        // }
+        // digits.inrt(digits.begin(),1);
+        // return digits;
+
+        int n=digits.size()-1;
+        for(int i=n; i>=0; i--){
             if(digits[i]<9){
                 digits[i]++;
                 return digits;
