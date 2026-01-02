c7add52248472da40adc0af1d3e87c5ef21e4a92
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-01-02 20:31:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (91.83%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
+    bool isVowel(char c){
+        c=tolower(c);
+        return 'a'==c || 'e'==c || 'i'==c || 'o'==c || 'u'==c;
+    }
     string reverseVowels(string s) {
         int i=0;
         int j=s.length()-1;
         while(i<j){
+            // s[i]!='a' && s[i]!='e' && s[i]!='i' && s[i]!='o' && s[i]!='u' &&
+            //     s[i]!='A' && s[i]!='E' && s[i]!='I' && s[i]!='O' && s[i]!='U' 
             if(
-                s[i]!='a' && s[i]!='e' && s[i]!='i' && s[i]!='o' && s[i]!='u' &&
-                s[i]!='A' && s[i]!='E' && s[i]!='I' && s[i]!='O' && s[i]!='U' 
+                !isVowel(s[i])
                 ){
                     i++;
                     continue;
                 }
+
+                // s[j]!='a' && s[j]!='e' && s[j]!='i' && s[j]!='o' && s[j]!='u' &&
+                // s[j]!='A' && s[j]!='E' && s[j]!='I' && s[j]!='O' && s[j]!='U'
             if(
-                s[j]!='a' && s[j]!='e' && s[j]!='i' && s[j]!='o' && s[j]!='u' &&
-                s[j]!='A' && s[j]!='E' && s[j]!='I' && s[j]!='O' && s[j]!='U'
+               !isVowel(s[j]) 
             ){
                 j--;
                 continue;
