beca4f0660c542a4ce5e94ded702e9e0ea4bf2b9
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-01-10 21:40:09 +0530

    Time: 15 ms (74.15%) | Memory: 18.1 MB (76.32%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -31,9 +31,45 @@ public:
         
     }
     int minimumDeleteSum(string s1, string s2) {
-        int n=s1.length();
-        int m=s2.length();
-        t.assign(n+1,vector<int>(m+1,-1));
-        return lcs(s1,s2,n,m);
+        // int n=s1.length();
+        // int m=s2.length();
+        // t.assign(n+1,vector<int>(m+1,-1));
+        // return lcs(s1,s2,n,m);
+        int n = s1.length();
+        int m = s2.length();
+        
+        // DP table of size (n+1) x (m+1)
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        
+        // 1. Fill Base Case: First Column (s2 is empty)
+        // Cost is sum of all characters in s1 so far
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = dp[i-1][0] + s1[i-1];
+        }
+        
+        // 2. Fill Base Case: First Row (s1 is empty)
+        // Cost is sum of all characters in s2 so far
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = dp[0][j-1] + s2[j-1];
+        }
+        
+        // 3. Fill the rest of the table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                
+                // If characters match, no deletion cost added for this step
+                if (s1[i-1] == s2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                } 
+                // If mismatch, delete the one that results in lower total cost
+                else {
+                    int deleteS1 = dp[i-1][j] + s1[i-1];
+                    int deleteS2 = dp[i][j-1] + s2[j-1];
+                    dp[i][j] = min(deleteS1, deleteS2);
+                }
+            }
+        }
+        
+        return dp[n][m];
     }
 };
