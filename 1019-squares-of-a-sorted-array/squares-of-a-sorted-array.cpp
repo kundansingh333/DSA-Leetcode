a1c670f79ceb0a35e9de3ba985428fed08fe92c6
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-01-29 10:49:53 +0530

    Time: 0 ms (100.00%) | Memory: 31.9 MB (6.56%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -2,11 +2,73 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         
+        // vector<int>ans;
+        // for(int i=0; i<nums.size(); i++){
+        //     nums[i]=nums[i]*nums[i];
+        // }
+        // sort(nums.begin(),nums.end());
+        // return nums;
+
+
+
+        int n = nums.size();
+        vector<int> neg;
+        vector<int> pos;
         vector<int>ans;
-        for(int i=0; i<nums.size(); i++){
-            nums[i]=nums[i]*nums[i];
+        for(int i=0; i<n; i++){
+            if(nums[i]>=0){
+                pos.push_back(nums[i]);
+            }else{
+                neg.push_back(nums[i]);
+            }
+
         }
-        sort(nums.begin(),nums.end());
+        reverse(neg.begin(),neg.end());
+        for(int i=0; i<neg.size(); i++){
+            neg[i]=neg[i]*neg[i];
+
+        }
+        for(int i=0; i<pos.size(); i++){
+            pos[i]=pos[i]*pos[i];
+            
+        }
+
+
+        int i=0;
+        int j=0;
+        int idx=0;
+        while(i<neg.size() && j<pos.size()){
+            if(neg[i]<=pos[j]){
+                // ans.push_back(neg[i]);
+                nums[idx]=neg[i];
+                i++;
+                idx++;
+            }else{
+                // ans.push_back(pos[j]);
+                nums[idx]=pos[j];
+                j++;
+                idx++;
+            }
+        }
+       
+        while(i<neg.size()){
+        //    ans.push_back(neg[i]);
+            nums[idx]=neg[i];
+                i++;
+                idx++;
+
+        }
+        while(j<pos.size()){
+            // ans.push_back(pos[j]);
+            nums[idx]=pos[j];
+                j++;
+                idx++;
+        }
         return nums;
+
+        
+        
+
+        
     }
 };
