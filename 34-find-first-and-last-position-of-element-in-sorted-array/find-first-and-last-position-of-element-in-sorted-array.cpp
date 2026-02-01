7024139631422cb6c3b21ce7f45428b75bfaf379
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-02-01 18:55:07 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (52.99%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,16 +1,51 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
+    //     int start=-1;
+    //     int end=-1;
+    //    for(int i=0; i<nums.size(); i++){
+    //         if(nums[i]==target && start == -1){
+    //             start=i;
+    //         }
+    //         if(nums[i]==target){
+    //             end=i;
+    //         }
+    //     }
+    //     return {start,end};
+
+        int low=0;
+        int high=nums.size()-1;
         int start=-1;
-        int end=-1;
-       for(int i=0; i<nums.size(); i++){
-            if(nums[i]==target && start == -1){
-                start=i;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                start=mid;
+                high=mid-1;
             }
-            if(nums[i]==target){
-                end=i;
+            else if(nums[mid]<target){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        
+        int n_low=0;
+        int n_high=nums.size()-1;
+        int end=-1;
+        while(n_low<=n_high){
+            int mid=n_low+(n_high-n_low)/2;
+            if(nums[mid]==target){
+                end=mid;
+                n_low=mid+1;
+            }
+            else if(nums[mid]<target){
+                n_low=mid+1;
+            }else{
+                n_high=mid-1;
             }
         }
         return {start,end};
+
+
     }
 };
