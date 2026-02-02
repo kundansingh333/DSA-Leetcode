6aa416a74300855c89f6a9ccd525859fe0ef7f3f
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-02-02 23:45:52 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (33.45%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -3,40 +3,66 @@ public:
     int search(vector<int>& nums, int target) {
 
 
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]==target){
-                return i;
+        // for(int i=0; i<nums.size(); i++){
+        //     if(nums[i]==target){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+
+
+
+
+
+
+
+
+
+
+        int low=0;
+        int high=nums.size()-1;
+        int n=nums.size();
+        int idx=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>nums[n-1]){
+                low=mid+1;
+            }else{
+                idx=mid;
+                high=mid-1;
+            }
+        }
+        int f_low=0;
+        int f_high=idx-1;
+
+        while(f_low<=f_high){
+            int mid=f_low+(f_high-f_low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]<target){
+                f_low=mid+1;
+            }else{
+                f_high=mid-1;
+            }
+        }
+
+
+        int s_low=idx;
+        int s_high=n-1;
+
+
+        while(s_low<=s_high){
+            int mid=s_low+(s_high-s_low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]<target){
+                s_low=mid+1;
+            }else{
+                s_high=mid-1;
             }
         }
         return -1;
 
-
-
-
-
-
-
-
-
-        // int low=0;
-        // int high=nums.size()-1;
-        // int n=nums.size();
-        // while(low<=high){
-        //     int mid=low+(high-low)/2;
-        //     if(nums[mid]>nums[n-1]){
-        //         if(nums[mid]==target){
-        //             return mid;
-        //         }else{
-        //             low=mid+1;
-        //         }
-        //     }else{
-        //         if(nums[mid]==target){
-        //             return mid;
-        //         }else{
-        //             high=mid-1;
-        //         }
-        //     }
-        // }
-        // return -1;
     }
 };
