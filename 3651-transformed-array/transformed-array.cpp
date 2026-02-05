9df0131973f664eb2cfb5ae06f571fb2dc1125e6
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-02-05 20:10:40 +0530

    Time: 7 ms (54.87%) | Memory: 25.6 MB (70.75%) - LeetSync

--- 3651-transformed-array/transformed-array.cpp
+++ 3651-transformed-array/transformed-array.cpp
@@ -5,20 +5,23 @@ public:
         vector<int>result(n);
         for(int i=0; i<nums.size(); i++){
             // int index;
-            if(nums[i]>0){
-                int index = i+nums[i];
-                index = index % n;
-                result[i]=nums[index];
-            }else if(nums[i]<0){
-                int index=(i+nums[i]);
-                index= index % n;
-                if(index<0){
-                    index+=n;
-                }
-                result[i]=nums[index];
-            }else{
-                result[i]=nums[i];
-            }
+            // if(nums[i]>0){
+            //     int index = i+nums[i];
+            //     index = index % n;
+            //     result[i]=nums[index];
+            // }else if(nums[i]<0){
+            //     int index=(i+nums[i]);
+            //     index= index % n;
+            //     if(index<0){
+            //         index+=n;
+            //     }
+            //     result[i]=nums[index];
+            // }else{
+            //     result[i]=nums[i];
+            // }
+
+            int index=(i+nums[i]%n+n)%n;
+            result[i]=nums[index];
             
         }
         return result;
