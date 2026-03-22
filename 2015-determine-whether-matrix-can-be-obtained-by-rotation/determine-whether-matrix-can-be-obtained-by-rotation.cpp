aac85ddac8873a8c24339fbb599185703329d77f
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-03-22 10:12:43 +0530

    Time: 3 ms (2.82%) | Memory: 15 MB (14.32%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        if(mat==target){
+            return true;
+        }
         int m=mat.size();
         int n= mat[0].size();
         vector<vector<int>>transposed(n,vector<int>(m,0));
         int count=0;
-        while(count<4){
+        while(count<3){
             for(int i=0; i<m; i++){
                 for(int j=0; j<n; j++){
                     transposed[i][j]=mat[j][i];
