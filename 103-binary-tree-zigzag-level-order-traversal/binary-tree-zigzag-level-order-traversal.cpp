faca8871c52fa1643d7dc31a1fceb7fe771af68c
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-03-26 21:36:42 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (17.62%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -20,12 +20,20 @@ public:
         q.push(root);
         bool isLeft=1;
         while(!q.empty()){
-            vector<int>temp;
+            // vector<int>temp;
+            
             int lvlsize=q.size();
+            vector<int>temp(lvlsize);
+            int i=0;
+            int originalsize=lvlsize;
             while(lvlsize--){
                 TreeNode* f=q.front();
                 q.pop();
-                temp.push_back(f->val);
+                // temp.push_back(f->val);
+                int index=isLeft?i:(originalsize-1-i);
+                temp[index]=f->val;
+                
+
                 if(f->left!=nullptr){
                     q.push(f->left);
 
@@ -33,15 +41,18 @@ public:
                 if(f->right!=nullptr){
                     q.push(f->right);
                 }
+                i++;
             }
-            if(isLeft){
-                ans.push_back(temp);
-                isLeft=1-isLeft;
-            }else{
-                reverse(temp.begin(),temp.end());
-                ans.push_back(temp);
-                isLeft=1-isLeft;
-            }
+            isLeft=1-isLeft;
+            // if(isLeft){
+            //     ans.push_back(temp);
+            //     isLeft=1-isLeft;
+            // }else{
+            //     reverse(temp.begin(),temp.end());
+            //     ans.push_back(temp);
+            //     isLeft=1-isLeft;
+            // }
+            ans.push_back(temp);
         }
         return ans;
     }
