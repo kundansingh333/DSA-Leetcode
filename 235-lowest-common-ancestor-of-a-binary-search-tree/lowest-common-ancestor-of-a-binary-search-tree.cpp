3a9e59abee014899d09358ed62630f768b1349cd
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-03-28 15:00:43 +0530

    Time: 27 ms (22.52%) | Memory: 23.6 MB (34.02%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,25 +11,45 @@
 class Solution {
 public:
     TreeNode* ans=nullptr;
-    int fun(TreeNode* root,TreeNode*p,TreeNode* q){
-        if(root==nullptr){
-            return 0;
+    // int fun(TreeNode* root,TreeNode*p,TreeNode* q){
+    //     if(root==nullptr){
+    //         return 0;
+    //     }
+    //     int left=fun(root->left,p,q);
+    //     int right=fun(root->right,p,q);
+    //     int self=0;
+    //     if(root == p || root==q){
+    //         self=1;
+    //     }
+    //     int total=left+right+self;
+    //     if(total==2 && ans==nullptr){
+    //         ans=root;
+    //         // return 
+    //     }
+    //     return total;
+    // }
+
+    void fun(TreeNode* root,int p, int q){
+        if(root == nullptr){
+            return;
         }
-        int left=fun(root->left,p,q);
-        int right=fun(root->right,p,q);
-        int self=0;
-        if(root == p || root==q){
-            self=1;
-        }
-        int total=left+right+self;
-        if(total==2 && ans==nullptr){
+        if(root->val == p || root->val==q){
             ans=root;
-            // return 
+            return;
+        }else if(root->val < p){
+            fun(root->right,p,q);
+        }else if(root->val > q){
+            fun(root->left,p,q);
+        }else{
+            ans=root;
+            return;
         }
-        return total;
+        return;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        int a=fun(root,p,q);
+        int maxi=max(p->val,q->val);
+        int mini=min(p->val,q->val);
+        fun(root,mini,maxi);
         return ans;
     }
 };
