8fbf8c53305880d63b44aa7a3b36b59d1d89e84f
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-03-31 17:01:21 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (44.99%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -11,24 +11,39 @@
  */
 class Solution {
 public:
-    vector<int> res;
+    // vector<int> res;
+    TreeNode* prev=nullptr;
+    bool ans=true;
+
     void inOrder(TreeNode* root){
         // left-root-right
         if(root==nullptr){
             return ;
         }
         inOrder(root->left);
-        res.push_back(root->val);
+        // res.push_back(root->val);
+        if(prev==nullptr){
+            prev=root;
+        }else{
+            if(prev->val >=root->val){
+                ans=false;
+                prev=root;
+                return;
+            }
+        }
+        prev=root;
         inOrder(root->right);
         return;
     }
     bool isValidBST(TreeNode* root) {
         inOrder(root);
-        for(int i=0; i<res.size()-1; i++){
-            if(res[i]>=res[i+1]){
-                return false;
-            }
-        }
-        return true;
+        // for(int i=0; i<res.size()-1; i++){
+        //     if(res[i]>=res[i+1]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        return ans;
+
     }
 };
