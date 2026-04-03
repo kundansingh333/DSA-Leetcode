392ed73ccfb15d9005359c55637f0e393de2632c
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-04-03 22:25:44 +0530

    Time: 302 ms (5.03%) | Memory: 407.2 MB (5.05%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -18,15 +18,49 @@ public:
         }
     }
     bool canPartition(vector<int>& nums) {
+        // int n=nums.size();
+        // int sum=0;
+        // for(auto it:nums){
+        //     sum+=it;
+        // }
+        // if(sum%2!=0){
+        //     return false;
+        // }else{
+        //     return fun(nums,sum/2,n);
+        // }
+
+        //top-down approach
         int n=nums.size();
         int sum=0;
-        for(auto it:nums){
+        for(auto it: nums){
             sum+=it;
         }
         if(sum%2!=0){
-            return false;
-        }else{
-            return fun(nums,sum/2,n);
+            return 0;
         }
+        int s=sum/2;
+        vector<vector<int>>t(n+1,vector<int>(s+1));
+        //initialization
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<s+1; j++){
+                if(i==0){
+                    t[i][j]=0;
+                }
+                if(j==0){
+                    t[i][j]=1;
+                }
+            }
+        }
+        //choice diagram
+        for(int i=1; i<t.size(); i++){
+            for(int j=1; j<t[0].size(); j++){
+                if(nums[i-1]<=j){
+                    t[i][j]=t[i-1][j-nums[i-1]] || t[i-1][j];
+                }else{
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+        return t[n][s];
     }
 };
