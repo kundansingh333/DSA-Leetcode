efaa6b53ed3b9d53be31f80e030d1872d696f9a5
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-04-09 22:21:03 +0530

    Time: 119 ms (5.18%) | Memory: 294.7 MB (5.98%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -18,9 +18,37 @@ public:
 
     }
     int longestCommonSubsequence(string text1, string text2) {
+        // int n=text1.length();
+        // int m=text2.length();
+        // return lcs(text1,text2,n,m);
+
         int n=text1.length();
         int m=text2.length();
-        return lcs(text1,text2,n,m);
+
+        vector<vector<int>>t(n+1,vector<int>(m+1));
+
+        //initialization
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<m+1; j++){
+                if(i==0 || j==0){
+                    t[i][j]=0;
+                }
+            }
+        }
+
+        //choice diagram
+        for(int i=1; i<t.size(); i++){
+            for(int j=1; j<t[0].size(); j++){
+                if(text1[i-1]==text2[j-1]){
+                    t[i][j]=1+t[i-1][j-1];
+                }else{
+                    t[i][j]=max(t[i][j-1],t[i-1][j]);
+                }
+            }
+        }
+        return t[n][m];
+
+
         
     }
 };
