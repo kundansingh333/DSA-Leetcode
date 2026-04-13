dd206e33216e27ab5adaea2664385fd26be1faa8
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-04-13 14:39:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (5.18%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        if(s.length()==0) return true;
-        int s_p=0;
-        int t_p=0;
-
-        while(t_p<t.length() && s_p<s.length()){
-            if(t[t_p]==s[s_p]){
-                s_p++;
-            }
-            t_p++;
-            if(s_p==s.length()){
-                return true;
+        int n=s.length();
+        int m=t.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1));
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<m+1; j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                }
             }
         }
-            
-        
-        return false;
+        for(int i=1; i<dp.size(); i++){
+            for(int j=1; j<dp[0].size(); j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+
+        // int mini=min(n,m);
+        if(n==dp[n][m]){
+            return true;
+        }else{
+            return false;
+        }
         
     }
 };
