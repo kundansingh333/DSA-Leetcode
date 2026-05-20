0e7bb0e344fbca73c54836fc609b3c2d2a9586af
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-05-20 12:16:25 +0530

    Time: 503 ms (69.56%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select 
+    p.project_id,
+    round(sum(e.experience_years)/count(p.project_id),2) as average_years
+from Project p
+left join Employee e on e.employee_id=p.employee_id
+group by p.project_id;
