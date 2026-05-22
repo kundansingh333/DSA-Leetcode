80b622c0008f1d8e629f89d768473c9124caf325
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-05-22 09:42:48 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (33.81%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,12 +1,38 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        // int low=0;
+        // int n=nums.size();
+        // int high=n-1;
+        // while(low<=high){
+        //     int mid=low+(high-low)/2;
+        //     if(nums[mid]==target) {
+        //         return mid;
+        //     }else if(nums[low]<=nums[mid]){
+        //         if(nums[low]<=target && nums[mid]>=target){
+        //             high=mid-1;
+        //         }else{
+        //             low=mid+1;
+        //         }
+        //     }else{
+        //         if(nums[high]>=target && nums[mid]<=target){
+        //             low=mid+1;
+        //         }else{
+        //             high=mid-1;
+        //         }
+        //     }
+
+
+        // }
+        // return -1;
+
+
         int low=0;
         int n=nums.size();
         int high=n-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(nums[mid]==target) {
+            if(nums[mid]==target){
                 return mid;
             }else if(nums[low]<=nums[mid]){
                 if(nums[low]<=target && nums[mid]>=target){
@@ -15,14 +41,12 @@ public:
                     low=mid+1;
                 }
             }else{
-                if(nums[high]>=target && nums[mid]<=target){
+                if(nums[mid]<=target && nums[high]>=target){
                     low=mid+1;
                 }else{
                     high=mid-1;
                 }
             }
-
-
         }
         return -1;
     }
