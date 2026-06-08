50a38330fcfdd7b8ae2dd190b9e9131ba5aee069
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-06-08 09:18:52 +0530

    Time: 2 ms (89.93%) | Memory: 127.7 MB (78.53%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,23 +1,48 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
+        // int n=nums.size();
+        // vector<int>ans;
+        // for(int i=0; i<n; i++){
+        //     if(nums[i]<pivot){
+        //         ans.push_back(nums[i]);
+        //     }
+        // }
+
+        // for(int i=0; i<n; i++){
+        //     if(nums[i]==pivot){
+        //         ans.push_back(nums[i]);
+        //     }
+        // }
+
+        // for(int i=0; i<n; i++){
+        //     if(nums[i]>pivot){
+        //         ans.push_back(nums[i]);
+        //     }
+        // }
+        // return ans;
+
         int n=nums.size();
-        vector<int>ans;
-        for(int i=0; i<n; i++){
-            if(nums[i]<pivot){
-                ans.push_back(nums[i]);
+        int countLess=0;
+        int countEqual=0;
+        for(auto num:nums){
+            if(num<pivot){
+                countLess++;
+            }else if(num==pivot){
+                countEqual++;
             }
         }
-
-        for(int i=0; i<n; i++){
-            if(nums[i]==pivot){
-                ans.push_back(nums[i]);
-            }
-        }
-
-        for(int i=0; i<n; i++){
-            if(nums[i]>pivot){
-                ans.push_back(nums[i]);
+        int less_ptr=0;
+        int equal_ptr=countLess;
+        int great_ptr=countLess+countEqual;
+        vector<int>ans(n);
+        for(auto num:nums){
+            if(num<pivot){
+                ans[less_ptr++]=num;
+            }else if(num==pivot){
+                ans[equal_ptr++]=num;
+            }else{
+                ans[great_ptr++]=num;
             }
         }
         return ans;
