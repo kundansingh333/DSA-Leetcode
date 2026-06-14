1e9927d7dd4536b7da49672673a54f72f5521377
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-06-14 11:52:55 +0530

    Time: 2 ms (70.05%) | Memory: 124.3 MB (67.45%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -11,25 +11,54 @@
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        int sum=0;
-        ListNode* temp=head;
-        vector<int>arr;
-        while(temp!=nullptr){
-            arr.push_back(temp->val);
-            temp=temp->next;
-        }
-        int i=0;
-        int j=arr.size()-1;
-        int maxi=0;
-        while(i<j){
-            int sum=arr[i]+arr[j];
-            maxi=max(maxi,sum);
-            // return sum;
-            i++;
-            j--;
+        // int sum=0;
+        // ListNode* temp=head;
+        // vector<int>arr;
+        // while(temp!=nullptr){
+        //     arr.push_back(temp->val);
+        //     temp=temp->next;
+        // }
+        // int i=0;
+        // int j=arr.size()-1;
+        // int maxi=0;
+        // while(i<j){
+        //     int sum=arr[i]+arr[j];
+        //     maxi=max(maxi,sum);
+        //     // return sum;
+        //     i++;
+        //     j--;
+
+        // }
+        // return maxi;
+
+
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
 
         }
-        return maxi;
+
+        ListNode* prev=nullptr;
+        ListNode* curr=slow;
+        while(curr!=nullptr){
+            ListNode* nextNode=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nextNode;
+        }
+        ListNode* firstHalf=head;
+        ListNode* secondHalf=prev;
+        int maxSum=0;
+        while(secondHalf!=nullptr){
+            int curr_sum=firstHalf->val+secondHalf->val;
+            maxSum=max(maxSum,curr_sum);
+            secondHalf=secondHalf->next;
+            firstHalf=firstHalf->next;
+        }
+        return maxSum;
+
         
     }
 };
