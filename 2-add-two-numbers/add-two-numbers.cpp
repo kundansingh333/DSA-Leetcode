636c990d75d2799ee79fb5e5947a049a83b794e3
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-07-15 21:40:41 +0530

    Time: 0 ms (100.00%) | Memory: 77.1 MB (45.70%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -0,0 +1,74 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    // ListNode* reverseLL(ListNode* head){
+    //     ListNode* prev=nullptr;
+    //     ListNode* curr=head;
+    //     ListNode* next=nullptr;
+    //     while(curr!=nullptr){
+    //         next=curr->next;
+    //         curr->next=prev;
+    //         prev=curr;
+    //         curr=next;
+    //     }
+    //     return prev;
+    // }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        // l1=reverseLL(l1);
+        // l2=reverseLL(l2);
+
+        // ListNode* dummy=new ListNode(-1);
+        // ListNode* curr=dummy;
+        // int carry=0;
+        // while(l1!=nullptr || l2!=nullptr || carry!=0){
+        //     int sum=carry;
+        //     if(l1!=nullptr){
+        //         sum+=l1->val;
+        //         l1=l1->next;
+        //     }
+        //     if(l2!=nullptr){
+        //         sum+=l2->val;
+        //         l2=l2->next;
+        //     }
+        //     carry=sum/10;
+        //     curr->next=new ListNode(sum%10);
+        //     curr=curr->next;
+        // }
+        // ListNode* res=dummy->next;
+        // delete dummy;
+        // return res;
+
+
+        ListNode* dummy=new ListNode(-1);
+        ListNode* current=dummy;
+        int carry=0;
+        while(l1!=nullptr || l2!=nullptr || carry!=0){
+            int sum=carry;
+            if(l1!=nullptr){
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2!=nullptr){
+                sum+=l2->val;
+                l2=l2->next;
+            }
+
+            carry=sum/10;
+            ListNode* temp=new ListNode(sum%10);
+            current->next=temp;
+            current=temp;
+        }
+        ListNode* res=dummy->next;
+        delete dummy;
+        return res;
+    }
+};
