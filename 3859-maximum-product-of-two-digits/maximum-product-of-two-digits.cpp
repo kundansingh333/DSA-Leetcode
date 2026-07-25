ed076f245d463b416454e982ec822f0878870832
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-07-25 11:41:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (89.66%) - LeetSync

---
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findMaxEle(int n){
+        long long maxDigit=INT_MIN;
+        long long secondmaxDigit=INT_MIN;
+        while(n){
+            int last_digit = n%10;
+             if((int)maxDigit <= last_digit){
+                secondmaxDigit = maxDigit;
+                maxDigit=last_digit;
+             }
+         else if((int)secondmaxDigit < last_digit && last_digit < (int)maxDigit){
+                 secondmaxDigit = last_digit;
+             }
+             n=n/10;
+        }
+
+        //if(secondmaxDigit==INT_MIN) return maxDigit * maxDigit;
+        return secondmaxDigit*maxDigit;
+    }
+    int maxProduct(int n) {
+        int num=n;
+        long long mxEle = findMaxEle(n);
+        return mxEle;
+    }
+};
