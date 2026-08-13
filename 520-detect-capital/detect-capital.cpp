10728d4b3cdaeddb5243e69e28fc59a20a2789ff
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-08-13 20:38:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (80.11%) - LeetSync

---
+++ 520-detect-capital/detect-capital.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count=0;
+        for(auto ch:word){
+            if(isupper(ch)){
+                count++;
+            }
+        }
+        if(count==word.length()){
+            return true;
+        }
+        if(count==0){
+            return true;
+
+        }
+        if(count==1 && isupper(word[0])){
+            return true;
+        }
+        return false;
+    }
+};
