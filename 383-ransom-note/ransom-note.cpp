97f45fd9d8c24d061b964de06d3aa3a9fffce1a1
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-08-24 22:09:43 +0530

    Time: 4 ms (38.47%) | Memory: 12.2 MB (12.29%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int>mp1;
+        unordered_map<char,int>mp2;
+        for(auto ch:magazine){
+            mp1[ch]++;
+        }
+        for(auto &ch:ransomNote){
+            mp2[ch]++;
+        }
+
+        for(auto it=mp2.begin(); it!=mp2.end(); it++){
+            if(mp1[it->first]<it->second) return false;
+        }
+        return true;
+
+
+
+    }
+};
