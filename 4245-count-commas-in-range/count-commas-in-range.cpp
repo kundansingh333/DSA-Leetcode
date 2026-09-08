ff7436d5f718c166426a86633e86ae66af6a9985
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-09-08 20:32:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (15.51%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int countCommas(int n) {
-        string s=to_string(n);
-        int len=s.length();
-        int count=0;
-        while(len>=4){
-            count++;
-            n--;
-            s=to_string(n);
-            len=s.length();
-        }
-        return count;
+        // string s=to_string(n);
+        // int len=s.length();
+        // int count=0;
+        // while(len>=4){
+        //     count++;
+        //     n--;
+        //     s=to_string(n);
+        //     len=s.length();
+        // }
+        // return count;
+        return max(0,n-999);
     }
 };
