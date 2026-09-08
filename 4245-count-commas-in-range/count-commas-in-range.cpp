278149da33b62da11ec2a6e658084c4943f935e3
Kundan Kumar Singh <kundankumarsingh2005@gmail.com>
2026-09-08 20:30:00 +0530

    Time: 736 ms (5.13%) | Memory: 9.5 MB (5.52%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        string s=to_string(n);
+        int len=s.length();
+        int count=0;
+        while(len>=4){
+            count++;
+            n--;
+            s=to_string(n);
+            len=s.length();
+        }
+        return count;
+    }
+};
